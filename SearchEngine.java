9ede612f543e21c2e8cc01a8f04d1f6e50007f1f
cdeliivanov <cdeliivanov@ucsd.edu>
2023-04-24 10:37:35 -0700

    search engine

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,11 +1,9 @@
 import java.io.IOException;
 import java.net.URI;
 
-public class SearchEngine implements URLHandler{
-    
-    public String handleRequest(URI url) {
-        String command = url.getPath().split("?")[0];
-        
-    }
+// public class SearchEngine implements URLHandler{
 
-}
+    
+    
+
+// }
